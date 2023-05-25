46ccc120acee4c209a0ad8b590742e2d63b197ce
GabrieleGrossi <grossigabriele98@gmail.com>
2023-05-25 16:09:09 +0200

    create the function

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const clock = setInterval(lesson, 1000);
+/*const clock = setInterval(lesson, 1000);
+
 function lesson() {
-    alert('New Lesson');
+    alert('New lesson');
+    clearInterval(clock);
 }
-clearInterval(clock);
+alert('test')
+*/
+
+function updateCountdown() {
+    const now = new Date();
+    const alarm = new Date();
+
+    alarm.setHours(9);
+    alarm.setMinutes(30);
+    alarm.setSeconds(0);
+
+    
+    }
+  
+    updateCountdown();
